4bc84e8c79997fcdd46bd76e9f310ece916b5123
Herald Felisilda <herald_felisilda@yahoo.com>
2026-02-11 23:23:09 +0800

    Update Added leaking entry correction

--- application/views/admin-includes/navigation.php
+++ application/views/admin-includes/navigation.php
@@ -303,12 +303,13 @@
                     <?php if((array_key_exists('admin',$roleResponsible) && ($roleResponsible['admin'] == 1) ) || ($this->session->userdata('usertype') == 'admin')){ ?>
 					<li class="<?php if($this->uri->segment(2)=='responsibilities'){echo 'active open';}?>
 									<?php if($this->uri->segment(2)=='addbillingperiod'){echo 'active open';}?>
-									<?php if($this->uri->segment(2)=='createbalanceforward'){echo 'active open';}?>
+								   <?php if($this->uri->segment(2)=='createbalanceforward'){echo 'active open';}?>
 								   <?php if($this->uri->segment(2)=='employee_logins'){echo 'active open';}?>
 								   <?php if($this->uri->segment(2)=='mobilenotifications'){echo 'active open';}?>
 								   <?php if($this->uri->segment(2)=='classification_category'){echo 'active open';}?>
 								   <?php if($this->uri->segment(2)=='classification'){echo 'active open';}?>
-								   <?php if($this->uri->segment(2)=='amountrate'){echo 'active open';}?>">
+								   <?php if($this->uri->segment(2)=='amountrate'){echo 'active open';}?>
+								   <?php if($this->uri->segment(2)=='Leakingentrycorrection'){echo 'active open';}?>">
 						<a href="#"><i class="fa fa-lg fa-fw fa-user"></i> <span class="menu-item-parent">Admin</span></a>
 						<ul>
 
@@ -343,6 +344,9 @@
 							</li>
 							<li class="<?php if($this->uri->segment(2)=='addmetercustomerreading' && $this->uri->segment(3)=='edit'){echo 'active';}?>">
 								<a href="<?php echo ADMIN_URL;?>addmetercustomerreading/edit"> Meter Reading Correction</a>
+							</li>
+							<li class="<?php if($this->uri->segment(2)=='Leakingentrycorrection'){echo 'active';}?>">
+								<a href="<?php echo ADMIN_URL;?>Leakingentrycorrection"> Leaking Entry Correction</a>
 							</li>
 							
 							<?php if((array_key_exists('database_backup',$roleResponsible) && ($roleResponsible['database_backup'] == 1 || (is_array($roleResponsible['database_backup']) && count($roleResponsible['database_backup']) > 0)) ) || ($this->session->userdata('usertype') == 'admin')){ ?>
