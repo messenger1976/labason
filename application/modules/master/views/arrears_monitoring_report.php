26bc29c3dabcf80327e57cd717830ccc062bfee5
Herald Felisilda <herald_felisilda@yahoo.com>
2026-05-06 00:16:11 +0800

    Update Added Arrears monitoring

---
+++ application/modules/master/views/arrears_monitoring_report.php
@@ -0,0 +1,344 @@
+<!DOCTYPE html>
+	
+<!-- MAIN PANEL -->
+		<div id="main" role="main">
+
+			<!-- RIBBON -->
+			<div id="ribbon">
+
+				<span class="ribbon-button-alignment"> 
+					<span id="refresh" class="btn btn-ribbon" data-action="resetWidgets" data-title="refresh"  rel="tooltip" data-placement="bottom" data-original-title="<i class='text-warning fa fa-warning'></i> Warning! This will reset all your widget settings." data-html="true">
+						<i class="fa fa-refresh"></i>
+					</span> 
+				</span>
+
+				<!-- breadcrumb -->
+				<ol class="breadcrumb">
+					<li><a href="<?php echo ADMIN_URL;?>">Home</a></li>
+					<li><a href="<?php echo ADMIN_URL;?>reports/arrears_monitoring_report/">Arrears Monitoring</a></li>
+					<li>search</li>
+				</ol>
+				
+			</div>
+			<!-- END RIBBON -->
+
+			<!-- MAIN CONTENT -->
+			<div id="content">
+
+				<div class="row">
+					<div class="col-xs-12 col-sm-7 col-md-7 col-lg-4">
+						<h1 class="page-title txt-color-blueDark"><i class="glyphicon glyphicon-search"></i>Display <span>>  Arrears Monitoring Report </span></h1>
+					</div>
+					<div class="col-xs-12 col-sm-5 col-md-5 col-lg-8">
+						<ul id="sparks" class="">
+							<li class="sparks-info">
+							<?php 
+							     $income1 = $this->comm_model->get_income_metercustomer();
+							     extract($income1);
+								 $income2 = $this->comm_model->get_income_monthlycustomer();
+								 extract($income2);
+								 $intotal = $total1 + $total2;
+							?>
+								<h5> Income <span class="txt-color-blue">PHP <?php print_r(number_format($intotal,2));?></span></h5>
+								<div class="sparkline txt-color-blue hidden-mobile hidden-md hidden-sm">
+									
+								</div>
+							</li>
+							<?php
+							     $expense1 = $this->my_model->get_outcome_expenses();
+							     extract($expense1);
+								 $expense2 = $this->my_model->get_outcome_payroll();
+								 extract($expense2);
+								 $extotal = $extotal1 + $extotal2;
+							?>
+							<li class="sparks-info">
+								<h5> Expense <span class="txt-color-purple">PHP <?php print_r(number_format($extotal,2));?></span></h5>
+								<div class="sparkline txt-color-purple hidden-mobile hidden-md hidden-sm">
+									
+								</div>
+							</li>
+							<?php 
+							     $total_customer = $this->my_model->total_customer();
+							     extract($total_customer); 
+							?>
+							<li class="sparks-info">
+								<h5> Total Customer <span class="txt-color-greenDark">&nbsp;<?php print_r($count_id);?></span></h5>
+								<div class="sparkline txt-color-greenDark hidden-mobile hidden-md hidden-sm">
+									
+								</div>
+							</li>
+						</ul>
+					</div>
+				</div>
+				<!-- widget grid -->
+				<section id="widget-grid" class="">
+
+					<!-- row -->
+
+					<div class="row">
+
+						<!-- a blank row to get started -->
+						<div class="col-sm-6 col-lg-12">
+						
+
+								<!-- your contents here -->
+								<div class="panel panel-default">
+									
+									<div class="widget-body">
+				
+										
+										  	
+											<?php if($msg != ''){?>
+											<div class="alert alert-block alert-success">
+												<button type="button" class="close" data-dismiss="alert">
+												<i class="icon-remove"></i>
+												</button>
+												<p>
+													<i class="icon-ok"></i>
+													<?php echo $msg?$msg:'';?>
+												</p>
+											</div>
+											<?php } ?>	
+											
+											<fieldset>
+														<legend>
+															Arrears Monitoring -Search 
+															<div  class="pull-right" style="padding-right:20px;">
+																<button type="submit" class="btn btn-sm btn-primary" name="display" id="display" style="margin-bottom: 5px;">Display</button>
+															</div>
+														</legend>
+														<div class="form-group col-lg-6">
+															<div class="col-lg-12 controls">
+																<div class="form-group">
+																	<span class="input-group-addon"><i class="icon-user"></i><strong>As of Date:</strong></span>
+																	<input class="form-control"  type="text" id="asofdate" name="asofdate"  placeholder="DD-MM-YYYY" value="" required>
+																</div>
+															</div>
+														</div>
+                                                        <div class="form-group col-lg-6">
+															<div class="col-lg-12 controls">
+																<div class="form-group">
+																	<span class="input-group-addon"><i class="icon-user"></i><strong> Zone:</strong></span>
+																	<select class="form-control" name="zone" id="zone" required>
+																		<option value="0">--All--</option>
+																		<?php foreach($zone as $key => $value){ ?>
+																		<option value="<?php echo $value['id'];?>"><?php echo $value['zone'];?></option>
+																		<?php } ?>
+																	</select>
+																</div>
+															</div>
+														</div>
+														<div class="form-group col-lg-6">
+                                                            <div class="col-lg-12 controls">
+                                                                <div class="form-group">
+                                                                <span class="input-group-addon"><i class="icon-user"></i><strong>Status : </strong></span>
+                                                                    <select  class="form-control" name="status" id="status" required>
+                                                                        <option value="">--All--</option>
+                                                                        <option value="1">Active</option>
+                                                                        <option value="0">Inactive</option>
+																		<option value="2">Disconnected</option>
+                                                                        
+                                                                    </select>
+                                                                </div>
+                                                            </div>
+                                                        
+                                                        </div>
+														<div style="clear:both"></div>
+														
+														<div class="col-xs-12" id="paidcustomerDiv" style="margin-top: 13px;"></div>
+															
+													</fieldset>
+													
+												</div> 
+														
+													    
+															
+												
+														
+														
+														
+										
+									</div>
+								    
+								
+								</div>	
+						</div>
+                        	
+				        </div>
+					</div>
+                    <!-- end row -->
+
+				</section>
+				<!-- end widget grid -->
+
+					
+
+				</section>
+				<!-- end widget grid -->
+
+			</div>
+			<!-- END MAIN CONTENT -->
+
+		</div>
+		<!-- END MAIN PANEL -->
+		
+
+		<?php include('footer.php');?>
+		<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+	</body>
+
+</html>
+
+<!-- PAGE RELATED PLUGIN(S) -->
+		<script src="<?php echo base_url();?>js/plugin/datatables/jquery.dataTables.min.js"></script>
+		<script src="<?php echo base_url();?>js/plugin/datatables/dataTables.colVis.min.js"></script>
+		<script src="<?php echo base_url();?>js/plugin/datatables/dataTables.tableTools.min.js"></script>
+		<script src="<?php echo base_url();?>js/plugin/datatables/dataTables.bootstrap.min.js"></script>
+		<script src="<?php echo base_url();?>js/plugin/datatable-responsive/datatables.responsive.min.js"></script>
+		<script type="text/javascript">
+		
+		// DO NOT REMOVE : GLOBAL FUNCTIONS!
+		
+		$(document).ready(function() {
+			
+			pageSetUp();
+			
+			/* // DOM Position key index //
+		
+			l - Length changing (dropdown)
+			f - Filtering input (search)
+			t - The Table! (datatable)
+			i - Information (records)
+			p - Pagination (paging)
+			r - pRocessing 
+			< and > - div elements
+			<"#id" and > - div with an id
+			<"class" and > - div with a class
+			<"#id.class" and > - div with an id and class
+			
+			Also see: http://legacy.datatables.net/usage/features
+			*/	
+	
+			/* BASIC ;*/
+				var responsiveHelper_dt_basic = undefined;
+				var responsiveHelper_datatable_fixed_column = undefined;
+				var responsiveHelper_datatable_col_reorder = undefined;
+				var responsiveHelper_datatable_tabletools = undefined;
+				
+				var breakpointDefinition = {
+					tablet : 1024,
+					phone : 480
+				};
+	
+				$('#dt_basic').dataTable({
+					"sDom": "<'dt-toolbar'<'col-xs-12 col-sm-6'f><'col-sm-6 col-xs-12 hidden-xs'l>r>"+
+						"t"+
+						"<'dt-toolbar-footer'<'col-sm-6 col-xs-12 hidden-xs'i><'col-xs-12 col-sm-6'p>>",
+					"autoWidth" : true,
+			        "oLanguage": {
+					    "sSearch": '<span class="input-group-addon"><i class="glyphicon glyphicon-search"></i></span>'
+					},
+					"preDrawCallback" : function() {
+						// Initialize the responsive datatables helper once.
+						if (!responsiveHelper_dt_basic) {
+							responsiveHelper_dt_basic = new ResponsiveDatatablesHelper($('#dt_basic'), breakpointDefinition);
+						}
+					},
+					"rowCallback" : function(nRow) {
+						responsiveHelper_dt_basic.createExpandIcon(nRow);
+					},
+					"drawCallback" : function(oSettings) {
+						responsiveHelper_dt_basic.respond();
+					}
+				});
+	
+			/* END BASIC */
+			
+			/* COLUMN FILTER  */
+		    var otable = $('#datatable_fixed_column').DataTable({
+				"sDom": "<'dt-toolbar'<'col-xs-12 col-sm-6 hidden-xs'f><'col-sm-6 col-xs-12 hidden-xs'<'toolbar'>>r>"+
+						"t"+
+						"<'dt-toolbar-footer'<'col-sm-6 col-xs-12 hidden-xs'i><'col-xs-12 col-sm-6'p>>",
+				"autoWidth" : true,
+				"oLanguage": {
+					"sSearch": '<span class="input-group-addon"><i class="glyphicon glyphicon-search"></i></span>'
+				},
+				"preDrawCallback" : function() {
+					if (!responsiveHelper_datatable_fixed_column) {
+						responsiveHelper_datatable_fixed_column = new ResponsiveDatatablesHelper($('#datatable_fixed_column'), breakpointDefinition);
+					}
+				},
+				"rowCallback" : function(nRow) {
+					responsiveHelper_datatable_fixed_column.createExpandIcon(nRow);
+				},
+				"drawCallback" : function(oSettings) {
+					responsiveHelper_datatable_fixed_column.respond();
+				}		
+			
+		    });
+		    
+		    $("div.toolbar").html('<div class="text-right"><img src="img/logo.png" alt="SmartAdmin" style="width: 111px; margin-top: 3px; margin-right: 10px;"></div>');
+		    	   
+		    $("#datatable_fixed_column thead th input[type=text]").on( 'keyup change', function () {
+		    	
+		        otable
+		            .column( $(this).parent().index()+':visible' )
+		            .search( this.value )
+		            .draw();
+		            
+		    } );
+		    /* END COLUMN FILTER */   
+	    
+		})
+
+		</script>
+		
+<script type="text/javascript">
+var curDate = '<?php echo date('d-m-Y') ?>';	
+function fun_calendor(field){
+	$("#"+field).focus();
+} 
+
+$(document).ready(function(){
+	$("#asofdate").datepicker({
+		showAnim: null,
+		dateFormat: 'dd-mm-yy',
+		buttonImage: '/images/calender.jpg',
+		buttonImageOnly: true,
+		firstDay: 1,
+		nextText: '',
+		prevText: '',
+		numberOfMonths: [1, 1],
+	});
+
+    $('#display').on('click', function(evt){
+        evt.preventDefault();
+       
+        var asofdate = $("#asofdate").val();
+        var zone = $("#zone").val();
+		var status = $("#status").val();
+
+		if(asofdate === ''){
+			alert("Please select As of Date");
+			return false;
+		}
+
+        showSpinner();
+        
+        $.ajax({
+            
+            type : "POST",
+            url	: '<?php echo ADMIN_URL;?>reports/getarrearsmonitoringsearch',
+            
+            data	: "asofdate="+asofdate+"&zone="+zone+"&status="+status,
+            complete: function(data){
+                var op = data.responseText.trim();
+                $("#paidcustomerDiv").html(op);
+                hideSpinner();
+            }
+        });
+    });
+});
+
+</script>	
